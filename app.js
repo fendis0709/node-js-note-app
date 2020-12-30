f884b3831b639eea0ac712834a9a00e847adcd4f
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2020-12-30 15:26:52 +0700

    Install package from NPM (Challenge S3 - E5)

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+const chalk = require('chalk')
 const notes = require('./notes')
 const validator = require('validator')
 
 console.log(notes.getNotes())
 
-console.log(validator.isEmail('fendi@doktr.me'))
+let email = 'fendi@doktr.me'
+console.log(`Is "${email}" a valid email?`)
+console.log(validator.isEmail(email))
+
+let message = 'Success!'
+console.log(chalk.green(message))
+console.log(chalk.bgGreen(message))
+console.log(chalk.bold.bgYellow(message))
+console.log(chalk.inverse.yellow(message))
